6221bc4d615f8994ac258119a7b06ed0fdad3c85
“mtstechnologies” <michael.trindade@hotmail.com>
2024-09-29 16:07:26 -0400

    Configuring routes

--- movie-ui/src/App.tsx
+++ movie-ui/src/App.tsx
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 
 
 function App() {
   return (
     <>
-      <Navbar /> {/*essa posicao garante que toda pagina ira carregar o header*/}
+      <BrowserRouter>{/*aqui é iniciado a configuracao de ativacao das rotas*/}
+        <Navbar />{/*essa posicao garante que toda pagina ira carregar o header*/}
+        <Routes>
+          <Route path="/" element={<Listing />} />
+          <Route path="/form">
+            <Route path=":movieId" element={<Form />} />{/*sub-rota para pegar o parametro*/}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
